cd9fd3e89ea750731925d9fd42d00e97dd553318
ecksi <jimecksi@gmail.com>
2020-03-23 20:59:26 -0600

    Add Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="../assets/yo.png" alt="Thumbs up from your homie">
+    <div>
+      <h2>Jim Dechant</h2>
+      <div>Software Developer</div>
+      <div>jimecksi@gmail.com</div>
+      <div>https://github.com/Ecksi</div>
+      <div>https://www.linkedin.com/in/jimecksi/</div>
+    </div>
+  </div>  
+</template>
+
+<script>
+  export default {
+    name: 'Card'
+  }
+</script>
